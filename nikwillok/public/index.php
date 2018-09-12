bf5267508fd8b47485eb37e5a498e09576d3db54
Nik O'Keefe <nikokeefe3@gmail.com>
2018-09-12 16:05:22 +1200

    Update public page

--- nikwillok/public/index.php
+++ nikwillok/public/index.php
@@ -2,11 +2,12 @@
 
 <html lang="en">
     <head>
-        <title></title>
+        <title>NikWillOK</title>
         <meta charset="utf-8">
     </head>
 
     <body>
+        <h1>Nik will be here soon</h1>
 
     </body>
 </html>
