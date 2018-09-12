8c71ec07461a67aab055095953429ab3240d0085
Nik O'Keefe <nikokeefe3@gmail.com>
2018-09-12 16:11:52 +1200

    Updated admin area

---
+++ nikwillok/public/admin/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <title>Backend</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <header>
+            <h1>Admin Area</h1>
+        </header>
+
+        <navigation>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+        </navigation>
+
+        <footer>
+            &copy <?php echo date('Y'); ?> nikwillok
+        </footer>
+
+    </body>
+</html>
