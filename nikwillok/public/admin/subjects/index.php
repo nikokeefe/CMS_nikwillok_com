22751ee55ed497110fb85c9242b63fa25d705737
Nik O'Keefe <nikokeefe3@gmail.com>
2018-09-12 19:55:34 +1200

    Inserted subject table and dummy data

--- nikwillok/public/admin/subjects/index.php
+++ nikwillok/public/admin/subjects/index.php
@@ -1,11 +1,49 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php $page_title = 'Subjects'; ?>
+<?php 
+    $subjects = [
+        ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About'],
+        ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Career'],
+        ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Music'],
+        ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Work']
+    ];
+?>
 
+
+<?php $page_title = 'Subjects'; ?>
 <?php include(SHARED_PATH . '/admin_header.php'); ?>
 
 <div id="content">
+    <div class="subjects listing">
+        <h1>Subjects</h1>
 
+        <div class="actions">
+            <a class="action" href="">Create New Subject</a>
+        </div>
+
+        <table class="list">
+            <tr>
+                <th>ID</th>
+                <th>Position</th>
+                <th>Visible</th>
+                <th>Name</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+            </tr>
+
+            <?php foreach($subjects as $subject) { ?>
+                <tr>
+                    <td><?php echo $subject['id']; ?></td>
+                    <td><?php echo $subject['position']; ?></td>
+                    <td><?php echo $subject['visible'] == 1 ? 'true' : 'false'; ?></td>
+                    <td><?php echo $subject['menu_name']; ?></td>
+                    <td><a class="action" href="">View</a></td>
+                    <td><a class="action" href="">Edit</a></td>
+                    <td><a class="action" href="">Delete</a></td>
+                </tr>
+            <?php } ?>
+        </table>
 </div>
 
 <?php include(SHARED_PATH . '/admin_footer.php'); ?> 
